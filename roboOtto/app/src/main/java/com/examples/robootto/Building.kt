5d0482552ea790040740165f2bf035d6dea5b31a
Sergio Navarro <48925272+SerjoKraken@users.noreply.github.com>
2024-05-22 18:20:37 -0400

    Se agrega vista en construccion

--- roboOtto/app/src/main/java/com/examples/robootto/Building.kt
+++ roboOtto/app/src/main/java/com/examples/robootto/Building.kt
@@ -1,34 +1,20 @@
 package com.examples.robootto
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MenuSelector : AppCompatActivity() {
+class Building : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_menu_selector)
+        setContentView(R.layout.activity_building)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val option1 = findViewById<ImageView>(R.id.option1)
-
-        option1.setOnClickListener{
-            val intent = Intent(this, PistaBailable::class.java)
-            startActivity(intent)
-        }
-
-        val option2 = findViewById<ImageView>(R.id.option2)
-        option2.setOnClickListener {
-            startActivity(Intent(this, Building::class.java))
-        }
-
     }
 }
