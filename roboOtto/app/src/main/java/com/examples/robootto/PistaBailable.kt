09f173aa99660bb1ee584ea7f3099d942c2af4da
janunezg <103081788+janunezg@users.noreply.github.com>
2024-06-13 20:06:11 -0400

    new songs

--- roboOtto/app/src/main/java/com/examples/robootto/PistaBailable.kt
+++ roboOtto/app/src/main/java/com/examples/robootto/PistaBailable.kt
@@ -18,15 +18,15 @@ class PistaBailable : AppCompatActivity() {
 
     private var mediaPlayer: MediaPlayer? = null
     private val songMap = mapOf(
-        R.id.baile1 to R.raw.dance1,
-        R.id.baile2 to R.raw.dance2,
-        R.id.baile3 to R.raw.dance3,
-        R.id.baile4 to R.raw.dance4,
-        R.id.baile5 to R.raw.dance5,
-        R.id.baile6 to R.raw.dance6,
-        R.id.baile7 to R.raw.dance6,
-        R.id.baile8 to R.raw.dance6,
-        R.id.baile9 to R.raw.dance6
+        R.id.baile1 to R.raw.salsa,
+        R.id.baile2 to R.raw.tango,
+        R.id.baile3 to R.raw.rap,
+        R.id.baile4 to R.raw.pop,
+        R.id.baile5 to R.raw.cumbia,
+        R.id.baile6 to R.raw.breakdance,
+        R.id.baile7 to R.raw.rock,
+        R.id.baile8 to R.raw.jazz,
+        R.id.baile9 to R.raw.reggaeton
     )
     private val danceNames = mapOf(
         R.id.baile1 to "Salsa",
@@ -89,16 +89,12 @@ class PistaBailable : AppCompatActivity() {
 
         for (frame in frameLayouts) {
             frame.setOnClickListener {
-                // Quitar el fondo de todos los FrameLayout
                 frameLayouts.forEach { it.background = null }
-                // Establecer un borde alrededor del FrameLayout seleccionado
                 frame.background = ContextCompat.getDrawable(this, R.drawable.border)
 
-                // Parar la música actual si está reproduciéndose
                 mediaPlayer?.stop()
                 mediaPlayer?.release()
 
-                // Iniciar la nueva música
                 val songResId = songMap[frame.id]
                 if (songResId != null) {
                     mediaPlayer = MediaPlayer.create(this, songResId)
@@ -107,7 +103,6 @@ class PistaBailable : AppCompatActivity() {
                     stopButton.visibility = ImageButton.VISIBLE
                 }
 
-                // Actualizar el label con el nombre del baile seleccionado
                 val danceName = danceNames[frame.id]
                 selectedDanceLabel.text = danceName ?: "Seleccione un baile"
             }
